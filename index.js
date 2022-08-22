6fd5f3504d57effb1e65318d0b5fa2e49cf2facb
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-22 00:11:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ const client = new Client({
     ],
 });
 
+client.on('message', async (message) => {
+    if(message.channel.id === '1011138124723802132') {
+        message.channel.startTyping();
+fetch(`http://api.brainshop.ai/get?bid=168734&key=pT7Dflc5r4hPfnA1&uid=${uid}&msg=${msg}`)
+    .then(res => res.json())
+    .then(data => {
+        message.reply(`${data.cnt}`);
+    });
+      message.channel.stopTyping();
+    }
+});
 
 // Call .env file to get Token
 require('dotenv').config();
