60a610b527cf96e802c9e3c26e33cca99a992049
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-21 21:03:56 -0500

    Create guildMemberAdd.js

---
+++ src/events/example/guildMemberAdd.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'guildMemberAdd',
+
+    /**
+     * @param {GuildMember} member 
+     * @param {Client} client 
+     */
+    async execute(member, client) {
+        const { guild } = member;
+        console.log(`${member.username} join guilds ${guild.name}`);
+    }
+}
