c7bd937b75c067ad4646053f15acac9e0d7582a0
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-21 20:58:16 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botClientID: "1011082761093787758",
+    botPrefix: "s.",
+    ownerID: "794323707115470928",
+    embedColor: "#00dbff",
+    embedfooterText: "I am snek."
+}
