82c009ec62ea05c5c75dee76768cbff0d436e9f8
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-21 21:03:21 -0500

    Create ready.js

---
+++ src/events/client/ready.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+
+    /**
+     * @param {Client} client 
+     */
+    async execute(client) {
+        
+        // Puts an activity
+        client.user.setActivity("You through the tall grass.", {
+            type: "WATCHING",
+            name: "You through the tall grass."
+        });
+        
+        // Send a message on the console
+        console.log(`[LOG] ${client.user.tag} is now online!`);
+        console.log(`[LOG] Bot serving on Ready to serve in ${client.guilds.cache.size} servers`);
+        console.log(`[LOG] Bot serving ${client.users.cache.size} users`);
+    }
+}
