88c4ac7a44bb7fbfcfbdd61eed68d2a155e860ab
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-23 16:02:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ fetch(`http://api.brainshop.ai/get?bid=168734&key=${process.env.SNEKAPI}=${uid}&
 fetch(`http://api.brainshop.ai/get?bid=168734&key=${process.env.SNEKAPI2}=${uid}&msg=${message.content}`)
     .then(res => res.json())
     .then(data => {
-        return message.reply(`${data.cnt}`);
+        return message.reply(`${data.cnt} bro`);
     });
     }
 });
