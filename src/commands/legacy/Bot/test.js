db94a54d3ab8d6d3df92e187efb4e54a582ea8c7
The Sorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2022-08-22 21:33:08 -0500

    Update test.js

--- src/commands/legacy/Bot/test.js
+++ src/commands/legacy/Bot/test.js
@@ -1,10 +1,9 @@
-const { db } = require('quick.db');
-
 module.exports = {
     name: 'test',
     category: 'Bot',
     ownerOnly: false,
     run: async(client, message, args) => {
+        const db = require('quick.db');
         let cu = await db.get(`user_${message.author.id}_commands`);
 
         message.reply('Youve used, ' + `${cu}` + ' commands on snek.')
